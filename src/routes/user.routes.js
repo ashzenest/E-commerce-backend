6cc0d5f940ed0d20fb1cf2ec58df8b0d593e6805
Abdullah khan <khanabdullahdln86@gmail.com>
2026-02-15 07:12:49 +0530

    minor mistake

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser} from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -9,7 +10,7 @@ router.route("/login-user").post(loginUser)
 
 //SECURE ROUTE
 router.route("/refresh-tokens").post(refreshAccessToken)
-router.route("/logout-user").post(logoutUser)
-router.route("/change-password").post(changeCurrentPassword)
+router.route("/logout-user").post(verifyJWT, logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 
 export default router
